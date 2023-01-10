70b2b4935d7249b8b5dc16e4eaa8700074ca3877
sujin <ikosdu60@gmail.com>
2023-01-10 20:41:09 +0900

    Style: Home 컴포넌트 UI

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,7 +5,10 @@ import SearchResult from '../components/searchResult/SearchResult';
 function Home() {
   return (
     <div className={styles.container}>
-      <span>국내 모든 임상시험 검색하고 온라인으로 참여하기</span>
+      <span className={styles.title}>
+        <div>국내 모든 임상시험 검색하고</div>
+        <div>온라인으로 참여하기</div>
+      </span>
       <SearchBar />
       <SearchResult />
     </div>
