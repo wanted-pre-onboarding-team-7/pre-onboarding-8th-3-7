06541cd751dd0e1417c56cc6057dd4f310881c66
sujin <ikosdu60@gmail.com>
2023-01-10 21:30:32 +0900

    Style: SearchBar 컴포넌트 UI

--- src/components/searchBar/SearchBar.tsx
+++ src/components/searchBar/SearchBar.tsx
@@ -2,7 +2,13 @@ import styles from './SearchBar.module.css';
 
 function SearchBar() {
   //TODO: 검색창 기능 구현
-  return <div className={styles.container}></div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.searchIcon}>🔍</div>
+      <input className={styles.searchInput} type="text"></input>
+      <div className={styles.searchButton}>검색</div>
+    </div>
+  );
 }
 
 export default SearchBar;
