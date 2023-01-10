066fcb4245dbf2411ef0ebc916ac521350b71dd3
sujin <ikosdu60@gmail.com>
2023-01-10 22:22:40 +0900

    Style: SearchResult 컴포넌트 UI

--- src/components/searchResult/SearchResult.tsx
+++ src/components/searchResult/SearchResult.tsx
@@ -1,10 +1,51 @@
 import styles from './SearchResult.module.css';
 
+const Dummy = [
+  {
+    sickCd: 'A00',
+    sickNm: '콜레라',
+  },
+  {
+    sickCd: 'A01',
+    sickNm: '장티푸스 및 파라티푸스',
+  },
+  {
+    sickCd: 'A02',
+    sickNm: '기타 살모넬라 감염',
+  },
+  {
+    sickCd: 'A03',
+    sickNm: '시겔라증',
+  },
+  {
+    sickCd: 'A04',
+    sickNm: '기타 세균성 장 감염',
+  },
+  {
+    sickCd: 'A05',
+    sickNm: '달리 분류되지 않는 기타 세균성 음식매개중독',
+  },
+  {
+    sickCd: 'A06',
+    sickNm: '아메바증',
+  },
+  {
+    sickCd: 'A07',
+    sickNm: '기타 원충성 장 질환',
+  },
+];
 function SearchResult() {
-  //TODO: 검색 결과 기능 구현
   return (
     <div className={styles.container}>
-      <span>추천 검색어</span>
+      <div className={styles.recommentTitle}>추천 검색어</div>
+      <ul className={styles.keywordList}>
+        {Dummy.map((keyword) => (
+          <div className={styles.keywordWrapper}>
+            <div className={styles.searchIcon}>🔍</div>
+            <li className={styles.keyword}>{keyword.sickNm}</li>
+          </div>
+        ))}
+      </ul>
     </div>
   );
 }
