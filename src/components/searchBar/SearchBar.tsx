947c3d0ce1df2bf81adda8d3bedaf7c32a2679d3
sujin <soojin9877@naver.com>
2023-01-12 00:04:22 +0900

    Feat: UI 구현

--- src/components/searchBar/SearchBar.tsx
+++ src/components/searchBar/SearchBar.tsx
@@ -2,7 +2,16 @@ import styles from './SearchBar.module.css';
 
 function SearchBar() {
   //TODO: 검색창 기능 구현
-  return <div className={styles.container}></div>;
+  return (
+    <div className={styles.container}>
+      <input
+        type="text"
+        className={styles.barInput}
+        placeholder="질환명을 입력해 주세요"
+      ></input>
+      <button type="button" className={styles.barBtn}></button>
+    </div>
+  );
 }
 
 export default SearchBar;
