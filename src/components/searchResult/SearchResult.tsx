2552f43700422d7542aa882d8e479c8a3cc673b1
sujin <soojin9877@naver.com>
2023-01-12 01:10:35 +0900

    Feat: 검색어 하이라이팅 및 검색어 없음 표시

--- src/components/searchResult/SearchResult.tsx
+++ src/components/searchResult/SearchResult.tsx
@@ -13,15 +13,28 @@ interface Imap {
 function SearchResult({ keyword, recommendKeyword }: IKeywords) {
   return (
     <div className={styles.container}>
-      <h2>추천 검색어</h2>
-      <ul>
-        {recommendKeyword
-          .filter((_, i) => i < 10)
-          .map((i: Imap) => {
-            return <li key={i.sickCd}>{i.sickNm}</li>;
-          })}
-      </ul>
-      <p>검색어 없음</p>
+      {recommendKeyword.length === 0 ? (
+        <p>검색어 없음</p>
+      ) : (
+        <>
+          <h2>추천 검색어</h2>
+          <ul>
+            {recommendKeyword
+              .filter((_, i) => i < 10)
+              .map((i: Imap) => {
+                return i.sickNm.includes(keyword) ? (
+                  <li key={i.sickCd}>
+                    {i.sickNm.split(keyword)[0]}
+                    <span className={styles.highlightingText}>{keyword}</span>
+                    {i.sickNm.replace(i.sickNm.split(keyword)[0] + keyword, '')}
+                  </li>
+                ) : (
+                  <li key={i.sickCd}>{i.sickNm}</li>
+                );
+              })}
+          </ul>
+        </>
+      )}
     </div>
   );
 }
