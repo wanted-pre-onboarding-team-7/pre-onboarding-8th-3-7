319de1de2ce0555a05bdc63fb5f3d67e52f211ef
jiyoonZ <wldbsl416@gmail.com>
2023-01-12 12:12:45 +0900

    Feat : 키워드 볼드체로 변경 및 조건부 렌더링

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,18 @@
 import styles from './Home.module.css';
 import SearchBar from '../components/searchBar/SearchBar';
 import SearchResult from '../components/searchResult/SearchResult';
+import { useRecoilValue } from 'recoil';
+import { isInputFocus } from '../store/atoms';
 
 function Home() {
+  const isFocus = useRecoilValue(isInputFocus);
   return (
     <div className={styles.container}>
       <span className={styles.title}>
         국내 모든 임상시험 검색하고 온라인으로 참여하기
       </span>
       <SearchBar />
-      <SearchResult />
+      {isFocus && <SearchResult />}
     </div>
   );
 }
